30ae3ef9aac20ab98f48860f1526c612eb9b389e
Muhammed-M <58183087+Muhammed-M@users.noreply.github.com>
2025-09-10 00:24:11 +0300

    Update domino.py

--- domino.py
+++ domino.py
@@ -9,12 +9,7 @@ class Player() :
         self.p_set = [] 
 
     def __str__(self) :
-        return f"PLayer : {self.name} \nYour Set : {self.p_set}"
-
-
-
-
-domino_set1 = [(i, j) for i in range(6, -1, -1) for j in range(i, -1, -1)] 
+        return f"PLayer : {self.name} \nYour Set : {self.p_set}" 
 
         
 
@@ -55,7 +50,16 @@ class Game() :
 
 
 
-                
+domino_set1 = [(i, j) for i in range(6, -1, -1) for j in range(i, -1, -1)]  
+#[(6, 6), (6, 5), (6, 4), (6, 3), (6, 2), (6, 1), (6, 0), 
+#(5, 5), (5, 4), (5, 3), (5, 2), (5, 1), (5, 0), 
+#(4, 4), (4, 3), (4, 2), (4, 1), (4, 0), 
+#(3, 3), (3, 2), (3, 1), (3, 0), 
+#(2, 2), (2, 1), (2, 0), 
+#(1, 1), (1, 0), 
+#(0, 0)]
+
+
 
 def StartGame() :
     
@@ -154,4 +158,5 @@ def StartGame() :
 
 
 StartGame()
+
 
